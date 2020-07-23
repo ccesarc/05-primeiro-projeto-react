60d893366a5fab7da9a583daf7f8b16cbdbab47d
Caio ubuntu 2020 <caiocesarca@gmail.com>
2020-07-23 17:21:44 -0300

    Aula 5

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,21 +1,35 @@
-import React from 'react';
-
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from "../../services/api"
 
 import logoExplorer from '../../assets/img/github-explorer.svg.svg';
 
 import { Title, Form, Repositories } from './styles';
 
-
 //function Dashboard() { }
-
 const Dashboard: React.FC = () => {
+  //Criar um stado apenas para armazerar o valor do input
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, serRepositories] = useState([]);
+
+  function handleAddRepository(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    console.log('newRepo :', newRepo);
+    //adição de um novo repository
+    //consumir a api do github
+
+  }
+
   return (
     <>
       <img src={logoExplorer} alt="Git logo" />
       <Title>Dashboard Dashboard Dashboard </Title>
-      <Form >
-        <input type="text" placeholder="Digite o nome do repositorio" />
+      <Form onSubmit={handleAddRepository} >
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositorio"
+        />
         <button type="submit" > Pesquisar</button>
       </Form>
       <Repositories>
@@ -27,25 +41,6 @@ const Dashboard: React.FC = () => {
           </div>
           <FiChevronRight size={20} />
         </a>
-
-        <a href="Teste">
-          <img src="https://cdn.discordapp.com/avatars/521665962148102164/226f81ac772423a5ddc217c7f3786541" alt="Nome" />
-          <div>
-            <strong>Teste 123</strong>
-            <p>as asd asda sd asd asd asda sd </p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="Teste">
-          <img src="https://cdn.discordapp.com/avatars/521665962148102164/226f81ac772423a5ddc217c7f3786541" alt="Nome" />
-          <div>
-            <strong>Teste 123</strong>
-            <p>as asd asda sd asd asd asda sd </p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-
       </Repositories>
     </>
   );
